5de1bbce9428629b886f1e7e714a1f8d6d8c4c58
gsolovyev <fiddlestring@gmail.com>
2012-01-10 20:51:19 -0700

    update main page add location pages (region and uik) in anonymous mode

--- WebRoot/js/main.js
+++ WebRoot/js/main.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
 	$("#become_observer").button();
 	$("#become_voter").button();
 	$("#find_my_uik").button();
+	$("#login2").button();
     $("#login_dialog").dialog({width:300,height:150, modal: true});
 	$("#login_dialog").dialog("close");
-    $("#uik_search_dialog").dialog({width:650, height:250, modal: true});
-	$("#uik_search_dialog").dialog("close");
 	$("#login").click(function() {
+		//show login dialog
+		$("#login_dialog").dialog("open");
+	});
+	
+	$("#login2").click(function() {
 		//show login dialog
 		$("#login_dialog").dialog("open");
 	});
@@ -15,8 +19,12 @@ $(document).ready(function() {
 		//show login dialog
 		$("#login_dialog").dialog("open");
 	});
-	$("#find_my_uik").click(function() {$("#uik_search_dialog").dialog("open");});
-	$("#find_my_ui_link").click(function() {$("#uik_search_dialog").dialog("open");});
-	$("#find_uik_button").button();
 	$("#goto_region").button();
+	$("#goto_region2").button();
+	$("#goto_region").click(function() {
+		location.href="location_uik.html";
+	});
+	$("#goto_region2").click(function() {
+		location.href="location_uik.html";
+	});
  });

815a884978e2caea80ea74d5f52a60fd923ca6ca
serg <sergkop@gmail.com>
2012-01-22 08:05:00 +0000

    fixing buttons on location page

--- static/js/main.js
+++ static/js/main.js
@@ -26,18 +26,22 @@ function update_path(span_id, select_div_id){
     span.html("");
 
     var select_1 = $('#'+select_div_id+' [name="region_1"]');
-    var text1 = select_1.children('option[value="'+select_1.val()+'"]').text()
-    span.append($("<a/>").attr("href", location_url+select_1.val()).text(text1));
+    if (select_1.val()!=""){
+        var text1 = select_1.children('option[value="'+select_1.val()+'"]').text()
+        span.append($("<a/>").attr("href", location_url+select_1.val()).text(text1));
+    }
 
     var select_2 = $('#'+select_div_id+' [name="region_2"]');
-    var text2 = select_2.children('option[value="'+select_2.val()+'"]').text()
-    if (select_2.val()!="")
+    if (select_2.val()!=""){
+        var text2 = select_2.children('option[value="'+select_2.val()+'"]').text()
         span.append("&rarr;").append($("<a/>").attr("href", location_url+select_2.val()).text(text2));
+    }
 
     var select_3 = $('#'+select_div_id+' [name="region_3"]');
-    var text3 = select_3.children('option[value="'+select_3.val()+'"]').text()
-    if (select_3.val()!="")
+    if (select_3.val()!=""){
+        var text3 = select_3.children('option[value="'+select_3.val()+'"]').text()
         span.append("&rarr;").append($("<a/>").attr("href", location_url+select_3.val()).text(text3));
+    }
 }
 
 /* ---- Methods for manipulations with location selectors ---- */

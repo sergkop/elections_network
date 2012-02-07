bc9943567c8f7ee6499e256444c6c922600e43d2
serg <sergkop@gmail.com>
2012-02-07 07:06:28 +0000

    merged locations data

--- locations/models.py
+++ locations/models.py
@@ -1,39 +1,62 @@
 from django.db import models
 
+# Url templates for pages on izbirkom.ru with information about comissions
+RESULTS_ROOT_URL = r'http://www.%(region_name)s.vybory.izbirkom.ru/region/%(region_name)s?action=show&global=1&vrn=100100031793505&region=%(region_code)d&prver=0&pronetvd=null'
+RESULTS_URL = r'http://www.%(region_name)s.vybory.izbirkom.ru/region/%(region_name)s?action=show&global=true&root=%(root)d&tvd=%(tvd)d&vrn=100100031793505&prver=0&pronetvd=null&region=%(region_code)d&sub_region=%(region_code)d&type=0&vibid=%(tvd)d'
+
+#INFO_ROOT_URL = r'http://www.%(region_name)s.vybory.izbirkom.ru/region/%(region_name)s?action=show_komissia&region=%(region_code)d&sub_region=%(region_code)d&type=100&vrnorg=0&vrnkomis=0'
+INFO_URL = r'http://www.%(region_name)s.vybory.izbirkom.ru/region/%(region_name)s?action=show_komissia&region=%(region_code)d&sub_region=%(region_code)d&type=100&vrnorg=%(vrnorg)d&vrnkomis=%(vrnkomis)d'
+
 class Location(models.Model):
     """ The number of non-null values of parent specifies the level of location """
     # keys to the parents of the corresponding level (if present)
-    region = models.ForeignKey('self', null=True, blank=True, related_name='same_region_locations')
-    tik = models.ForeignKey('self', null=True, blank=True, related_name='same_tik_locations')
+    region = models.ForeignKey('self', null=True, blank=True, related_name='in_region')
+    tik = models.ForeignKey('self', null=True, blank=True, related_name='in_tik')
 
-    # TODO: how about region and it's code?
     name = models.CharField(max_length=150)
+    region_name = models.CharField(max_length=20)
+    region_code = models.IntegerField()
 
-    postcode = models.IntegerField()
-    address = models.CharField(max_length=200)
+    postcode = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True)
     telephone = models.CharField(max_length=50, blank=True)
     email = models.CharField(max_length=40, blank=True)
 
     # Ids required to access data from izbirkom.ru
     tvd = models.IntegerField()
     root = models.IntegerField()
-    vrnorg = models.IntegerField()
-    vrnkomis = models.IntegerField()
+    vrnorg = models.IntegerField(blank=True, null=True)
+    vrnkomis = models.IntegerField(blank=True, null=True)
 
     # Coordinates used in Yandex maps
     x_coord = models.FloatField(blank=True, null=True)
     y_coord = models.FloatField(blank=True, null=True)
 
-    # TODO: make name unique?
-    class Meta:
-        unique_together = ('name', 'region', 'tik')
+    def is_iks(self):
+        return self.region_id is None and self.tik is None
 
-    #def level(self):
-    #    # TODO: UIK, TIK or IKS
-    #    if self.region:
-    #        return 2
-    #    else:
-    #        return 1
+    def is_tik(self):
+        return self.region_id is not None and self.tik is None
+
+    def is_uik(self):
+        return self.region_id is not None and self.tik is not None
+
+    def results_url(self):
+        """ Link to the page with elections results on izbirkom.ru """
+        data = {'region_name': self.region_name, 'region_code': self.region_code}
+        if self.tvd != 0:
+            data.update({'tvd': self.tvd, 'root': self.root})
+            return RESULTS_URL % data
+        else:
+            return RESULTS_ROOT_URL % data
+
+    def info_url(self):
+        """ Link to the page with comission information on izbirkom.ru """
+        if self.vrnorg is None:
+            return ''
+
+        return INFO_URL % {'region_name': self.region_name, 'region_code': self.region_code,
+                'vrnorg': self.vrnorg, 'vrnkomis': self.vrnkomis}
 
     def __unicode__(self, full_path=False):
         name = self.name
--- maintenance/management/commands/init_locations.py
+++ maintenance/management/commands/init_locations.py
@@ -17,42 +17,54 @@ def print_progress(i, count):
 def data_path(region, type):
     return os.path.join(settings.PROJECT_PATH, 'data', 'regions', '%s-%s.json' % (region, type))
 
+def location_from_info(info):
+    from locations.models import Location
+    return Location(
+        name = info['name'],
+
+        postcode = info['postcode'],
+        address = info['address'],
+        telephone = info.get('telephone', ''),
+        email = info.get('email', ''),
+
+        x_coord = info.get('x_coord'),
+        y_coord = info.get('y_coord'),
+
+        vrnorg = int(info['vrnorg']) if 'vrnorg' in info else None,
+        vrnkomis = int(info['vrnkomis']) if 'vrnkomis' in info else None,
+        tvd = int(info.get('tvd', 0)),
+        root = int(info.get('root', 0)),
+    )
+
 class Command(BaseCommand):
     help = "Loads locations data after first syncdb."
 
     def handle(self, *args, **options):
-        region_ids = json.loads(open().read())
-
         REGIONS = {}
-        region_ids_path = os.path.join(settings.PROJECT_PATH, 'data', 'region_ids.json')
-        for line in open(regions_ids_path):
+        region_ids_path = os.path.join(settings.PROJECT_PATH, 'data', 'region_ids.txt')
+        for line in open(region_ids_path):
             region_title, region_name, region_id = line.split(', ')
-            REGIONS[region_name] = region_title
+            REGIONS[region_name] = (region_title, int(region_id.strip()))
 
         i = 0
         for region in REGIONS:
             print_progress(i, len(REGIONS))
 
-            # TODO: temporary check
-            if os.path.exists(data_path(region, 'merge')) and os.path.exists(data_path(region, 'center')):
-                info = json.loads(open(data_path(region, 'center')).read().decode('utf8'))
+            # Init regional comission
+            info = json.loads(open(data_path(region, 'center')).read().decode('utf8'))
+            regional_location = location_from_info(info)
+            regional_location.name = REGIONS[region][0]
+            regional_location.region_name = region
+            regional_location.region_code = REGIONS[region][1]
+            regional_location.save()
 
-                # TODO: take telephone from data files, not merged
-                Location(
-                    name = info['name'],
-
-                    postcode = info['postcode'],
-                    address = info['address'],
-                    telephone = info.get('telephone', ''),
-                    email = info.get('email', ''),
-
-                    x_coord = info.get('x_coord'),
-                    y_coord = info.get('y_coord'),
-
-                    vrnorg = int(info['vrnorg']),
-                    vrnkomis = int(info['vrnkomis']),
-                    tvd = int(info.get('tvd', 0)),
-                    root = int(info.get('root', 0)),
-                ).save()
+            # Init TIKs
+            info_list = json.loads(open(data_path(region, 'merge')).read().decode('utf8'))
+            for info in info_list:
+                tik = location_from_info(info)
+                tik.region = regional_location
+                tik.region_name = region
+                tik.region_code = REGIONS[region][1]
+                tik.save()
 
             i += 1

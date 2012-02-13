dca3fff549c30b8f3f422917b791d94b63c01f23
serg <sergkop@gmail.com>
2012-02-13 17:01:11 +0000

    reduce map data size

--- locations/templates/locations/map_data.js
+++ locations/templates/locations/map_data.js
@@ -1,8 +1,3 @@
-var electionCommissions = {};
-{% for location in all_locations %}
-    {% if location.x_coord %}
-        electionCommissions[{{ location.id }}] = new ElectionCommission({{ location.id }}, {{ location.level }}, "{{ location.name }}",
-        "{{ location.name }}", "{{ location.address }}", {{ location.x_coord|floatformat:10 }},
-        {{ location.y_coord|floatformat:10 }}, {numVoters: 500, numObservers: 5});
-    {% endif %}
-{% endfor %}
+var electionCommissions = {
+    {% for location in all_locations %}{% if location.x_coord %}{{ location.id }}: new ElectionCommission({{ location.id }},{{ location.level }},"{{ location.name }}","{{ location.name }}","{{ location.address }}",{{ location.x_coord|floatformat:10 }},{{ location.y_coord|floatformat:10 }},{numVoters:500,numObservers:5}){% if not forloop.last %},{% endif %}{% endif %}{% endfor %}
+};

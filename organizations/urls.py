39df2637a4a28529af52e606599184cc73e57a73
serg <sergkop@gmail.com>
2012-02-16 19:21:52 -0800

    fix url

--- organizations/urls.py
+++ organizations/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('organizations.views',
-    url(r'^(?P<name>[\w\.]+)$', 'organization_info', name='organization_info'),
-    url(r'^(?P<name>[\w\.]+)/edit$', 'edit_organization', name='edit_organization'),
-    url(r'^/create$', 'create_organization', name='create_organization'),
+    url(r'^organization/(?P<name>[\w\.]+)$', 'organization_info', name='organization_info'),
+    url(r'^organization/(?P<name>[\w\.]+)/edit$', 'edit_organization', name='edit_organization'),
+    url(r'^create_organization$', 'create_organization', name='create_organization'),
 )

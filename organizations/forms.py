aa5348d22ec773f5ef471a2cac951c9291172223
serg <sergkop@gmail.com>
2012-02-18 23:41:11 -0800

    commit

--- organizations/forms.py
+++ organizations/forms.py
@@ -6,13 +6,14 @@ from organizations.models import Organization
 
 class CreateOrganizationForm(forms.ModelForm):
     name = forms.RegexField(label=u'Идентификатор', max_length=20, min_length=2, regex=r'^[\w\.]+$',
-            help_text=u'Идентификатор может содержать от 4 до 20 символов (латинские буквы, цифры, подчеркивания и точки).')
+            help_text=u'Идентификатор может содержать от 4 до 20 символов (латинские буквы, цифры, подчеркивания и точки).' \
+                    u'<b>Этот идентификатор будет использован для формирования url страницы организации.</b>')
 
     helper = form_helper('', u'Создать')
 
     class Meta:
         model = Organization
-        exclude = ('verified', 'is_partner')
+        exclude = ('verified', 'is_partner', 'representative')
 
     def clean_about(self):
         return clean_html(self.cleaned_data['about'])

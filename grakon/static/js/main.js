d15833d3464663c1a6250304b410d0f3a6191238
serg <sergkop@gmail.com>
2012-02-20 19:10:09 -0800

    repair cookies problem

--- grakon/static/js/main.js
+++ grakon/static/js/main.js
@@ -301,3 +301,19 @@ function prevent_enter_work(selector){
         }
     });
 }
+
+function get_cookie(name){
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+};

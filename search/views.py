b6950cd223bb0baac14ad235fb2473ff492ce616
serg <sergkop@gmail.com>
2012-02-26 21:09:48 -0800

    bug fix

--- search/views.py
+++ search/views.py
@@ -154,8 +154,7 @@ class FindUikView(BaseSearchView):
     tab = 'find_uik'
 
     def get_context_data(self, **kwargs):
-        ctx = super(BaseSearchView, self).get_context_data(**kwargs)
-        ctx.update(self.preprocess())
+        ctx = super(FindUikView, self).get_context_data(**kwargs)
         return ctx
 
 find_uik = FindUikView.as_view()

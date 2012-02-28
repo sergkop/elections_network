8bd6ea1f4fecfc70aa1c756d0d163080ab731ab0
kalipso <kalipso@pretty-girl.ru>
2012-02-28 16:08:40 +0400

    Исправление багов под IE

--- grakon/static/js/iehacks.js
+++ grakon/static/js/iehacks.js
@@ -1,6 +1,7 @@
 $(function() {
+
     if (window.PIE) {
-        $('.ym-hlist ul li a, .ym-hlist ul li strong, .button, .breadcrumbs, .breadcrumbs ul li a, .breadcrumbs ul li strong, .breadcrumbs ul li a:focus, .breadcrumbs ul li a:hover').each(function() {
+        $('.ym-hlist ul li a, .ym-hlist ul li strong, .main-buttons div, .main-buttons-blue a, #login_btn, .breadcrumbs, .ui-button, .column_header, .ym-clearfix').each(function() {
             PIE.attach(this);
         });
     }
